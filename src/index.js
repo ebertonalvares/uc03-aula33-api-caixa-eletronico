26098c91252a4c6e765f24329abe8e5042dfa6b1
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2024-01-26 21:55:05 -0300

    inicio da rota de deposito

--- src/index.js
+++ src/index.js
@@ -77,7 +77,39 @@ app.get('/conta/:numero_conta/saldo', (request, response) => {
   }
 });
 
+// Rota depósito
+app.post('/conta/:numero_conta/deposito', (request, response) => {
+  const { numero_conta } = request.params;
 
+  const { valor, tipo } = request.body;
+
+  // validar se a conta existe
+  const contaEncontrada = contas.find(
+    (conta) => conta.numero_conta === numero_conta
+  )
+
+  if (!contaEncontrada) {
+    response.send({
+      "error": "Conta não encontrada"
+    })
+  }
+
+  // validar se o valor é positivo
+  if (!valor || valor <= 0) {
+    response.send({
+      "error": "Valor inválido"
+    })
+  }
+
+  // validar qual o tipo de depósito (DINHEIRO ou CHEQUE)
+  if (!tipo || typeof tipo !== String) {
+    response.send({
+      "error": "Tipo inválido"
+    })
+  } else if (tipo.toUpperCase() === 'DINHEIRO'){
+    // TODO - Validar se é inteiro
+  }
+})
 
 
 
