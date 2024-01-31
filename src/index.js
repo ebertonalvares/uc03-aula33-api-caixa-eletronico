c6b665f74dba6801fd61e33b78d78c0a9d30cdfe
eberton mauricio alvares <ebertonalvares@hotmail.com>
2024-01-30 21:52:40 -0300

    Alterado tipo para modulo e instalado nodemon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const { randomUUID } = require('crypto')
+import express from 'express'
+import { randomUUID } from 'node:crypto'
 
 const app = express()
 const port = 5000
